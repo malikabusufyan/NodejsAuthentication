f4f991c5d87ba209d0f3953a7785e867fe91f64a
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-05-23 01:41:33 +0530

    Basic Structure Done

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+//Use express router
+app.use("/", require("./routes"));
+
+//setup the view Engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error in running the server: ${err}`);
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+const homeController = require("../controllers/home_controller");
+
+console.log("Index from Routes");
+router.get("/", homeController.home);
+//To maintain all the list of users in the file
+router.use("/users", require("./users"));
+
+module.exports = router;
